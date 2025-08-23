cbf64d5320fcc80606d4ba446436cfef611da725
proyecto-eden-3-esferas <121116382+proyecto-eden-3-esferas@users.noreply.github.com>
2025-08-23 19:35:15 +0200

    2025-08-23 19:35

--- general.js
+++ general.js
@@ -13,6 +13,30 @@ function getInnerHTMLAttributeById (id) {
   return JSON.parse( document.getElementById(id).innerHTML );
 }
 
+/* Function hasContent(x) returns false if x is either null or undefined.
+ * It then returns true if:
+   x is a non-empty string or a non empty array, or
+   x is a non-empty object
+ * Last, if none of the preceding tests applies, return true.
+ */
+function hasContent(x) {
+  if(x == null   || typeof x == undefined)
+    return false;
+  if(typeof x == "string" || Array.isArray(x)) {
+    if(x.length > 0)
+      return true;
+    else
+      return false;
+  }
+  if(typeof x == "object")
+    return (Object.keys(x).length > 0);
+  return true;
+}
+
+function printObj(obj) {
+  return JSON.stringify(obj).replace("\"", "<strong>").replace("\"", "</strong>").replaceAll("\",\"", "<br/>").replaceAll("\"","").replace(/^{/, "").replace(/}$/,"");
+}
+
 
 
 function inPlaceMap(arr, func) {
